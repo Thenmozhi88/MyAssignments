3eb49a96d3dec4e96723fd0cb398e16c417a17e6
THENMOZHI UMAPATHY <84323603+Thenmozhi88@users.noreply.github.com>
2024-10-20 15:25:37 +0530

    Add files via upload
    var vs let vs const concept assignment

---
+++ HomeAssignment/day_01.js
@@ -0,0 +1,11 @@
+const browserVersion = "chrome"
+
+function getBrowserVersion() {
+    if (browserVersion == "chrome") {
+        let browserVersion
+
+    }
+    console.log(browserVersion)
+}
+
+getBrowserVersion()
